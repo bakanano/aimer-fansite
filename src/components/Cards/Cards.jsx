7435caac5e839d0ff7a49b5fda5313e7cff5ae4f
bakanano <narutoz0rs@live.com>
2023-06-28 15:51:29 +1200

    Add class names

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -3,14 +3,17 @@ import image01 from "../../assets/images/cards/image01.png";
 import image02 from "../../assets/images/cards/image02.jpg";
 import image03 from "../../assets/images/cards/image03.png";
 import image04 from "../../assets/images/cards/image04.png";
+import "./Cards.css";
 
 export default function Cards() {
     return (
-        <Link to="/aimer/gallery">
-            <img src={image01} alt="img1"/>
-            <img src={image02} alt="img2"/>
-            <img src={image03} alt="img3"/>
-            <img src={image04} alt="img4"/>
-        </Link>
+        <div className="cardsContainer">
+            <Link to="/aimer/gallery">
+                <img src={image01} alt="img1" class="image large"/>
+                <img src={image02} alt="img2" class="image large"/>
+                <img src={image03} alt="img3" class="image large"/>
+                <img src={image04} alt="img4" class="image large"/>
+            </Link>
+        </div>
     );
 }
